d60515524ef5152a9d86696bf4d00b2ef8b5801b
Ddiaz0102 <Ddiaz0102@DESKTOP-QI04I11>
2024-03-17 19:02:17 -0500

    segundo commit

--- src/shellSort/ShellSort.java
+++ src/shellSort/ShellSort.java
@@ -1,38 +1,51 @@
 package shellSort;
 
 public class ShellSort {
-    public static void main(String[] args) {
 
-        int[] array = {12, 34, 54, 2, 3, 21, 10, 31};
+    private static boolean less(int a, int b) {
+        return a < b;
+    }
+
+    private static void exch(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void sort(int[] arr) {
+        int n = arr.length;
+        
+        int intervalo = 1;
+        while (intervalo < n / 3) {
+            intervalo = 3 * intervalo + 1;
+        }
+
+        while (intervalo >= 1) {
+            for (int i = intervalo; i < n; i++) {
+                for (int j = i; j >= intervalo && less(arr[j], arr[j - intervalo]); j -= intervalo) {
+                    exch(arr, j, j - intervalo);
+                }
+            }
+            intervalo /= 3;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {12, 34, 54, 2, 3, 10, 20, 14};
 
         System.out.println("Array antes del ordenamiento:");
         imprimirArray(array);
-        
-        shellSort(array);
-        
+
+        sort(array);
+
         System.out.println("\nArray después del ordenamiento:");
         imprimirArray(array);
     }
 
-    static void shellSort(int arr[]) {
-        int n = arr.length;
 
-        for (int intervalo = n / 2; intervalo > 0; intervalo /= 2) {
-
-            for (int i = intervalo; i < n; i++) {
-                int temp = arr[i];
-                int j;
-                for (j = i; j >= intervalo && arr[j - intervalo] > temp; j -= intervalo) {
-                    arr[j] = arr[j - intervalo];
-                }
-                arr[j] = temp;
-            }
-        }
-    }
-
-    static void imprimirArray(int arr[]) {
-        int n = arr.length;
-        for (int i = 0; i < n; i++)
+    private static void imprimirArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
+        }
     }
 }
